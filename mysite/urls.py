e59c1bd3bcf6a82947f449674605232cdf8f35b4
suhailvs <suhailvs@gmail.com>
2026-07-05 10:43:02 +0530

    added media

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,4 @@ urlpatterns = [
     path("locations/add/", views.location_create, name="location_add"),
     path("locations/<int:pk>/", views.location_detail, name="location_detail"),
     path("locations/<int:pk>/edit/", views.location_edit, name="location_edit"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
